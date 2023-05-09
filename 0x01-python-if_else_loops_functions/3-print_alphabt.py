97782aad11fce4f89ac6567aab54d5ca10cdd53f
TRANDY-116 <randyfun16@gmail.com>
2023-05-09 15:17:06 -0700

    deci and hexa trick

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord("a"), ord("z") + 1):
-    # converting the codes in ASCII to characters
-    mini_letters = chr(letter)
-
-    if mini_letters == "q" or mini_letters == "e":
-        continue
-
-    print(mini_letters, end=" ")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
-    print(f"{number:d} = 0x{number:x}")
+    print("{} = {}".format(number, hex(number)))
