e7208b04c6631611c9777a6925157128af614917
TRANDY-116 <randyfun16@gmail.com>
2023-08-15 02:47:05 +0100

    new

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cisF = 'C is fun';
+const pyT = 'Python is cool';
+const jScr = 'JavaScript is amazing';
+
+console.log(cisF);
+console.log(pyT);
+console.log(jScr);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argL = process.argv.length;
+
+if (argL < 3) {
+	console.log('No argument');
+} else if (argL === 3) {
+	console.log('Arguement found');
+} else {
+	console.log('Argument Found');
+}
+
