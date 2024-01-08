46a74fa4453ade92ed32262958af93668af31507
TRANDY-116 <randyfun16@gmail.com>
2024-01-08 14:20:58 +0100

    first two

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+ * Code that prints a string
+ */
+
+const myVar = 'JavaScript is amazing';
+
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * Code that prints about 3 languages
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
