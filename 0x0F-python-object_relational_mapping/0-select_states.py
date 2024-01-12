8f15c0562f8992a9cbf5b09f5428460150122a84
TRANDY-116 <randyfun16@gmail.com>
2024-01-12 15:35:46 +0100

    first of many connections

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Python Script that lists all states from the db hbtn_0e_0usa """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    # Open connection
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+
+    # Execut:Wing query
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
