b95b9187223876c58d720a36471be3130f14e060
TRANDY-116 <randyfun16@gmail.com>
2024-01-30 22:52:52 +0100

    1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Script that fetches http://alx-intranet.hbtn.io/status """
+
+import urllib.request
+""" Importing the urrlib package """
+
+if __name__ == "__main__":
+    req = urllib.request. Request('https://alx-intranet.hbtn.io/status')
+
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        utf8 = the_page.decode("utf-8")
+
+        print("Body resonse:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(utf8))
