e76c9fde6b91a368bdf21dff0dde0bcf6e139f9c
TRANDY-116 <randyfun16@gmail.com>
2024-02-28 13:39:19 +0100

    fi6 indent 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
+    print("{:d}".format(matrix[i][j]), end="")
+    if j != (len(matrix[i]) - 1):
+        print(" ", end="")
         print("")
